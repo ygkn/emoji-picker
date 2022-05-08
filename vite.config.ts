4639329bb222e10083a155687695500b9301809a
ygkn <2000ygkn0713@gmail.com>
2022-05-08 09:52:45 +0900

    set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
+  test: {
+    includeSource: ['src/**/*.{ts,tsx}'],
+  },
 });
