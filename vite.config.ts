f888703a83c0ce1763cd78aa379c96978c9bbeaf
ygkn <2000ygkn0713@gmail.com>
2022-05-08 21:35:58 +0900

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
 import { defineConfig } from 'vitest/config';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 
+const base = process.env['NODE_ENV'] === 'development' ? '/' : '/emoji-picker/';
+
 export default defineConfig({
-  plugins: [react()],
+  base,
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['{twemoji,noto-emoji}/**/*.{png,svg}'],
+      manifest: {
+        name: 'Emoji Picker',
+        short_name: 'Emoji Picker',
+        start_url: base,
+        scope: base,
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icons/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/192-maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'icons/512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   define: {
     'import.meta.vitest': 'undefined',
   },
